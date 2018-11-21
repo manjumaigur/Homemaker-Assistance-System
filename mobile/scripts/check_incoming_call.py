29e755dbf85db605213c4b80b7a4e8451f712010
Manjunath Maigur <manjumbmb97@gmail.com>
2018-11-21 22:07:42 +0530

    small change

--- mobile/scripts/check_incoming_call.py
+++ mobile/scripts/check_incoming_call.py
@@ -1,16 +1,15 @@
-# import serial
-# import RPi.GPIO as GPIO
-# import os,time
+import serial
+import RPi.GPIO as GPIO
+import os,time
 
 def incoming_call():
-	# GPIO.setmode(GPIO.BOARD)
-	# port=serial.Serial("/dev/ttyS0",baudrate=9600,timeout=1)
-	# data = port.read(10)
-	# print(data)
-	# ring=str(data).find("RING")
-	# if ring>=0:
-	# 	print("incoming call....")
-	# 	return True
-	# else:
-	# 	return False
-	pass
+	GPIO.setmode(GPIO.BOARD)
+	port=serial.Serial("/dev/ttyS0",baudrate=9600,timeout=1)
+	data = port.read(10)
+	print(data)
+	ring=str(data).find("RING")
+	if ring>=0:
+		print("incoming call....")
+		return True
+	else:
+		return False
