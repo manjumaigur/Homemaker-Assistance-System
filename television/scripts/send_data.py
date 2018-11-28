0dd3774327627f6a8cdff419723fecf2c6cf3f3f
Manjunath Maigur <manjumbmb97@gmail.com>
2018-11-28 10:13:25 +0530

    changes

--- television/scripts/send_data.py
+++ television/scripts/send_data.py
@@ -6,10 +6,10 @@ def irSend(decimalCode):
 	HOST='192.168.43.164'
 	PORT=4210
 	data=decimalCode+"\r"
-	#s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	#read=s.connect((HOST,PORT))
+	s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	read=s.connect((HOST,PORT))
 	time.sleep(1)
-	#read=s.sendto(bytes(data,'ascii'),(HOST,PORT))
+	read=s.sendto(bytes(data,'ascii'),(HOST,PORT))
 	time.sleep(2)
 	print("sent")
-	#s.close()
+	s.close()
